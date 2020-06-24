3da19e89f9b05b592e9b20fc391253ae147f4277
Marco Alanis <maalanis@miners.utep.edu>
2020-06-24 03:15:31 -0600

    Initial Commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen';
+import SearchBar from './src/components/SearchBar';
+const navigator = createStackNavigator({
+    Search: SearchScreen
+},{
+    initialRoutName: 'Search',
+    defaultNavigationOptions: {
+        title: 'Business Search'
+    }
+
+});
+
+export default createAppContainer(navigator); 

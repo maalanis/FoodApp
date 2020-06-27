1abfcb2f72ec68d39b3c53eed5d96fbb61e0fa00
Marco Alanis <maalanis@miners.utep.edu>
2020-06-27 04:09:22 -0600

    seach API working

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,29 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
-
+import yelp from '../api/yelp';
+import UseResults from '../hooks/UseResults';
+import ResultsList from '../components/ResultsList';
+ 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [searchApi, results, error] = UseResults();
+    
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price;
+        });
+    };
+     
   return <View>
       <SearchBar term={term}
-                onTermChange={(newTerm) => setTerm(newTerm)} onTermSubmit={() => console.log("Term was submited")}/>
-      <Text> Search Screen</Text>
-   <Text> {term}</Text>
+                onTermChange={setTerm} 
+                onTermSubmit={() => searchApi(term)}/>
+                    {error ? <Text>{error}</Text> : null}
+   <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice('$')} title = "Cost Effective"/>
+      <ResultsList results={filterResultsByPrice('$$')} title = "Bit Pricier"/>
+      <ResultsList results={filterResultsByPrice('$$$')} title = "Big Spender"/>
       </View>  
 };
 

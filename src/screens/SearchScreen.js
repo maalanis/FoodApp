aa926a5a75488694f57f6084553dddf46e577fe1
Marco Alanis <maalanis@miners.utep.edu>
2020-07-13 00:18:52 -0600

    Added TouchOpacity and navigate option for each item

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,7 +5,7 @@ import yelp from '../api/yelp';
 import UseResults from '../hooks/UseResults';
 import ResultsList from '../components/ResultsList';
  
-const SearchScreen = () => {
+const SearchScreen = ({navigation }) => {
     const [term, setTerm] = useState('');
     const [searchApi, results, error] = UseResults();
     
@@ -15,18 +15,31 @@ const SearchScreen = () => {
         });
     };
      
-  return <View style = {{flex: 1}}>
+  return ( 
+      <>
       <SearchBar term={term}
                 onTermChange={setTerm} 
                 onTermSubmit={() => searchApi(term)}/>
                     {error ? <Text>{error}</Text> : null}
-   <Text>We have found {results.length} results</Text>
+  
     <ScrollView>
-      <ResultsList results={filterResultsByPrice('$')} title = "Cost Effective"/>
-      <ResultsList results={filterResultsByPrice('$$')} title = "Bit Pricier"/>
-      <ResultsList results={filterResultsByPrice('$$$')} title = "Big Spender"/>
+      <ResultsList 
+                    results={filterResultsByPrice('$')} 
+                    title = "Cost Effective" 
+                    navigation = {navigation}
+                />
+      <ResultsList 
+                    results={filterResultsByPrice('$$')} 
+                    title = "Bit Pricier" 
+                    navigation = {navigation}
+            />
+      <ResultsList 
+                    results={filterResultsByPrice('$$$')} 
+                    title = "Big Spender" 
+                    navigation = {navigation}/>
     </ScrollView>
-      </View>  
+      </>  
+        )
 };
 
 const styles = StyleSheet.create({});

e6d95e657416c17022e8c690f5192dd543b368be
Marco Alanis <maalanis@miners.utep.edu>
2020-07-16 22:15:25 -0600

    Added pictures from restaurant when displaying itemScreen

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {View,Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 import ResultsDetail from './ResultsDetail';
+import {withNavigation} from 'react-navigation';
 
 const ResultsList = ({title, results, navigation}) => {
+    if(!results.length) {
+        return null;
+    }
   return (
     <View style = {styles.container}>
       <Text style = {styles.title}>{title}</Text>
@@ -13,7 +17,7 @@ const ResultsList = ({title, results, navigation}) => {
         keyExtractor={(results) => results.id}
         renderItem={({item}) => {
             return ( 
-                <TouchableOpacity onPress ={() => navigation.navigate('ResultsShow')}>
+                <TouchableOpacity onPress ={() => navigation.navigate('ResultsShow', {id: item.id})}>
                     <ResultsDetail result={item}/>
                 </TouchableOpacity>
             );      
@@ -36,4 +40,4 @@ const styles = StyleSheet.create({
     
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,7 +5,7 @@ import yelp from '../api/yelp';
 import UseResults from '../hooks/UseResults';
 import ResultsList from '../components/ResultsList';
  
-const SearchScreen = ({navigation }) => {
+const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, error] = UseResults();
     
@@ -26,17 +26,18 @@ const SearchScreen = ({navigation }) => {
       <ResultsList 
                     results={filterResultsByPrice('$')} 
                     title = "Cost Effective" 
-                    navigation = {navigation}
+                    
                 />
       <ResultsList 
                     results={filterResultsByPrice('$$')} 
                     title = "Bit Pricier" 
-                    navigation = {navigation}
+                    
             />
       <ResultsList 
                     results={filterResultsByPrice('$$$')} 
                     title = "Big Spender" 
-                    navigation = {navigation}/>
+                    
+                    />
     </ScrollView>
       </>  
         )
